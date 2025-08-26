17b629e0785bd702462b2af3e02032e82a49a946
Genesis Lovino <i_genesis.lovino@stratpoint.com>
2025-08-26 15:29:15 +0800

    refactor: separated css colors

--- project/components/badge/ListBadge.tsx
+++ project/components/badge/ListBadge.tsx
@@ -14,12 +14,17 @@ interface ListBadgeProps {
 }
 
 const ListBadge: React.FC<ListBadgeProps> = ({ listName, listColor }) => {
+    const colorVariants = {
+        red: "bg-red-100 text-red-700",
+        orange: "bg-orange-100 text-orange-700",
+        yellow: "bg-yellow-100 text-yellow-700",
+        green: "bg-green-100 text-green-700",
+        blue: "bg-blue-100 text-blue-700",
+        violet: "bg-violet-100 text-violet-700",
+    };
+
     return (
-        <Badge
-            className={`capitalize border ${
-                listColor ? `bg-${listColor}-100 text-${listColor}-700` : ""
-            }`}
-        >
+        <Badge className={`capitalize border ${listColor ? colorVariants[listColor] : ""}`}>
             {listName}
         </Badge>
     );
