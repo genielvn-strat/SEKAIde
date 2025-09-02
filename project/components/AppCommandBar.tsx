bd48e3d2a86d3879784b84d67fcea35a6275a83a
Genesis Lovino <i_genesis.lovino@stratpoint.com>
2025-09-02 10:17:53 +0800

    fix: removed keyboard event

--- project/components/AppCommandBar.tsx
+++ project/components/AppCommandBar.tsx
@@ -18,20 +18,20 @@ export function AppCommandBar() {
     const { open, setOpen } = useSearchStore();
     const { recents } = useRecentStore();
 
-    useEffect(() => {
-        const down = (e: KeyboardEvent) => {
-            if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
-                e.preventDefault();
-                setOpen(!open);
-            }
-        };
-        document.addEventListener("keydown", down);
-        return () => document.removeEventListener("keydown", down);
-    }, []);
+    // useEffect(() => {
+    //     const down = (e: KeyboardEvent) => {
+    //         if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+    //             e.preventDefault();
+    //             setOpen(!open);
+    //         }
+    //     };
+    //     document.addEventListener("keydown", down);
+    //     return () => document.removeEventListener("keydown", down);
+    // }, []);
 
     return (
         <CommandDialog open={open} onOpenChange={setOpen} >
-            <CommandInput placeholder="Type a command or search..." />
+            <CommandInput placeholder="Search..." />
             <CommandList>
                 <CommandEmpty>No results found.</CommandEmpty>
                 {recents.length != 0 && (
